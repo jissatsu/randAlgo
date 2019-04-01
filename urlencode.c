83de79373d90af0221383a50b07a7682d0c7536f
scVnner <ichi256@abv.bg>
2019-04-01 13:06:49 +0300

    -fsfem

--- urlencode.c
+++ urlencode.c
@@ -1,45 +1,55 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-unsigned char * urlencode( unsigned char const *str );
+void freeString( void *str );
+unsigned char * urlencode( unsigned char const *url, size_t len );
 
-int main( void ) {
-	unsigned char url[0xFF] = "http://example.com.test/user?uid=1wha-tis that man_";
+int main( int argc, char **argv ) {
+	unsigned char url[] = "http://example.com.test/user?uid=1wha-tis that space_";
 	unsigned char *encoded;
-
-	encoded = urlencode( url );
+	
+	encoded = urlencode( url, strlen( url )	);
 	fprintf( stdout, "%s\n", encoded );
+	freeString( encoded );
+	
 	return 0;
 }
 
-unsigned char * urlencode( unsigned char const *str ) {
-	int i, j, k;
-	size_t len;
-	unsigned char *c;
-	static unsigned char *encoded;
+unsigned char * urlencode( unsigned char const *url, size_t len ) {
+	int i;
+	size_t actual_len   = strlen( url );
+	unsigned char *encoded;
 	char chars[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-	
-	i = j = 0;
-	len = strlen( str );
-	encoded = (unsigned char *) malloc( (len * 3) + 1 );
-	
-	while ( str[i] != '\x00' ) {
-		if ( str[i] == ' ' ) {
-			encoded[j++] = '+';
+
+	if ( len > 0 && len == actual_len ) {
+		encoded = (unsigned char *) malloc( (len * 3) + 1 );
+		if ( encoded ) {
+			i = 0;
+			while ( *url != '\x00' ) {
+				if ( *url == ' ' ) {
+					encoded[i++] = '+';
+				} 
+				else if ( (*url >= 'a' && *url <= 'z') || (*url >= 'A' && *url <= 'Z') || (*url >= '0' && *url <= '9') ||
+					*url == '~' || *url == '.' || *url == '-' || *url == '_') {
+					encoded[i++] = *url;
+				}
+				else{
+					encoded[i++] = '%';
+					encoded[i++] = toupper( chars[ *url >> 4 ] );
+					encoded[i++] = toupper( chars[ *url & 15 ] );
+				}
+				url++;
+			}
+			encoded[i] = '\x00';
+			return encoded;
 		}
-		else if ( (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z') ||
-			(str[i] >= '0' && str[i] <= '9') ||
-			 str[i] == '.' || str[i] == '~'  ||  str[i] == '-' || str[i] == '_' ) {
-			encoded[j++] = str[i];
-		} 
-		else{
-			encoded[j++] = '%';
-			encoded[j++] = toupper( chars[ str[i] >> 4 ] );
-			encoded[j++] = toupper( chars[ str[i] & 15 ] );
-		}
-		i++;
+		return (unsigned char *) "[Error] - Couldn't allocate memory for encoded string!\n";
 	}
-	encoded[j] = '\x00';
-	return encoded;
+	return (unsigned char *) "[Error] - The length provided is not the same as of the url!\n";
+}
+
+void freeString( void *str ) {
+	free( str );
 }
