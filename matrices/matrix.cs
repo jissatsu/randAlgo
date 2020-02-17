7356ac2f47fab29099f8ad42fb3b604eb02db5ee
killua <ichi256@abv.bg>
2020-02-17 10:33:19 +0200

    Add files via upload

---
+++ matrices/matrix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace zottt
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            int n         = int.Parse( Console.ReadLine() );
+            int[,] matrix = new int[n, n];
+            matrixx( n,  matrix );
+            show_matrix( matrix, n );
+        }
+
+        static void matrixx( int n, int[,] matrix )
+        {
+            for ( int i = 0 ; i < n ; i++ ) {
+                for ( int j = 0 ; j < n ; j++ ) {
+                    matrix[i, j] = n;
+                }
+            }
+        }
+
+        static void show_matrix( int[,] matrix, int n )
+        {
+            for ( int i = 0 ; i < n ; i++ ) {
+                for ( int j = 0 ; j < n ; j++ ) {
+                    Console.Write( $"{matrix[i, j]} " );
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
